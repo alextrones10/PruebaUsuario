be22b022813069921b464703289d32fa9223ad1c
alextrones10 <alexvivanco031@gmail.com>
2020-06-17 16:57:01 -0500

    Ho

---
+++ src/main/java/com/proyecto/entidad/Usuario.java
@@ -0,0 +1,117 @@
+package com.proyecto.entidad;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usu")
+	private int idUsuario;
+	
+	@Column(name = "pass_usu")
+	private String contraseña;
+	
+	@Column(name = "tdoc_usu")
+	private String tipodoc;
+	
+	@Column(name = "celu_usu")
+	private String celular;
+	
+	@Column(name = "naci_usu")
+	private String nacionalidad;
+	
+	@Column(name = "dept_usu")
+	private String departamento;
+	
+	@Column(name = "prov_usu")
+	private String provincia;
+	
+	@Column(name = "dist_usu")
+	private String distrito;
+	
+	@Column(name = "dire_usu")
+	private String direccion;
+
+	
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public String getTipodoc() {
+		return tipodoc;
+	}
+
+	public void setTipodoc(String tipodoc) {
+		this.tipodoc = tipodoc;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getDistrito() {
+		return distrito;
+	}
+
+	public void setDistrito(String distrito) {
+		this.distrito = distrito;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+}
